17e7ba326f866181aa19d2f978f5ab3cb00966de
lzwjava <lzwjava@gmail.com>
2016-08-26 03:30:13 +0800

    clean alipay code

--- application/config/alipay.php
+++ application/config/alipay.php
@@ -11,19 +11,7 @@ $config['partner'] = '2088421737526755';
 
 //商户的私钥,此处填写原始私钥去头去尾，RSA公私钥生成：
 //https://doc.open.alipay.com/doc2/detail.htm?spm=a219a.7629140.0.0.nBDxfy&treeId=58&articleId=103242&docType=1
-$config['private_key'] = 'MIICXAIBAAKBgQCswBq0v9sTzSuAptPy3++tM5vFDk6hpgyicp/66nPD83vzX4Et
-VwO1/tzwiKzaoYqNh2wiCnGsM42usg5VpxCS8FudzCWTiv4WmrTTHWV6v4VHGTM7
-x7+2FxxKCv15h/ltF8IQvTR0fUQpeOGSURtGLbcop1G45LG5NRQvsoV3iQIDAQAB
-AoGAbu92tvg10cOB2j5IAcI5AjsQHz2O1VPy7Sc7AUEqUn277ZA5PkqQwYEHlRfQ
-GR6AAV8j28BlOzZoF/WUKwD8LLH15J259VsgyUjymwxZsGqzaZf90CwKAPCUTF32
-AAVEPOpQMI1sWRFYjszSgO1MboraYqNThEbEDTuGIie8Z5kCQQDcWdgyPj3QRSaz
-/ftFqEGgia87adk9Q9YGqdXhZd3Vp8ljQRBLVK4waajVNP46aGhssWdn3zlTqV1x
-QFlovFwjAkEAyLLQWH+B4j9aGJf9xOcB1pxiO2RiqduZol2SZ4yZNQMSuYWFpeBe
-I/ejXRgAf0o7LX9HoWQrcJ8gVrfoKQUyYwJATVyZw+IIT7PHh8/2xRjymYtdY02h
-QJotbT95YN4jyRxUXy17rAb59PNTnC8zxovbhbWvKU+8z7uZm+ZMvbHL4wJASuDD
-8LCpwcKHF7PlPwg83Ff+IlCCqwK+YCBvFekT4/5ghwz03v1Fwj+LGJly3grQ51mc
-XO9lcaA8zX3RxTQmJwJBAJ7wAaMTTIUx4XMWHpwRksVjKyfS3PtNonndPLY1w/Di
-zjIuUa4Z/PBrPDK5Xw/7y91WUpIkQnkVzzoSky+ItSw=';
+$config['private_key'] = '';
 
 //支付宝的公钥，查看地址：https://openhome.alipay.com/platform/keyManage.htm?keyType=partner
 $config['alipay_public_key'] = 'MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCswBq0v9sTzSuAptPy3++tM5vFDk6hpgyicp/66nPD83vzX4EtVwO1/tzwiKzaoYqNh2wiCnGsM42usg5VpxCS8FudzCWTiv4WmrTTHWV6v4VHGTM7x7+2FxxKCv15h/ltF8IQvTR0fUQpeOGSURtGLbcop1G45LG5NRQvsoV3iQIDAQAB';
--- application/models/AlipayDao.php
+++ application/models/AlipayDao.php
@@ -42,7 +42,7 @@ class AlipayDao extends BaseDao
             $dataString = $this->makeParamString($order);
             $sign = $this->signData($dataString);
             $dataString .= '&sign_type="RSA"&sign="' . $sign . '"';
-            logInfo(json_encode(array("data" => $dataString)));
+            // logInfo(json_encode(array("data" => $dataString)));
             return $dataString;
         } else if ($channel == 'weiwin') {
 
@@ -59,13 +59,6 @@ class AlipayDao extends BaseDao
         return $sign;
     }
 
-    private function signData1($dataString)
-    {
-        $alipay_config = $this->config->item('alipay');
-        $rsa_sign = urlencode(rsaSign($dataString, $alipay_config['private_key']));
-        return $rsa_sign;
-    }
-
     private function makeParamString($array)
     {
         $quotes = array();
@@ -73,66 +66,6 @@ class AlipayDao extends BaseDao
             array_push($quotes, $key . '="' . $value . '"');
         }
         return implode($quotes, '&');
-    }
-
-    function sign_post()
-    {
-        date_default_timezone_set("PRC");
-        if ($this->checkIfParamsNotExist($this->post(), array('partner', 'service'))) {
-            return;
-        }
-        $partner = $this->post('partner');
-        $service = $this->post('service');
-        $alipay_config = $this->config->item('alipay');
-
-        if ($partner != $alipay_config['partner'] || $service != $alipay_config['service']) {
-            $this->failure(ERROR_PARTNER_OR_SERVICE);
-            return;
-        }
-        $data = createLinkstring($_POST);
-
-        //将待签名字符串使用私钥签名,且做urlencode. 注意：请求到支付宝只需要做一次urlencode.
-        $rsa_sign = urlencode(rsaSign($data, $alipay_config['private_key']));
-
-        //把签名得到的sign和签名类型sign_type拼接在待签名字符串后面。
-        $data = $data . '&sign=' . '"' . $rsa_sign . '"' . '&sign_type=' . '"' .
-            $alipay_config['sign_type'] .
-            '"';
-
-        //返回给客户端,建议在客户端使用私钥对应的公钥做一次验签，保证不是他人传输。
-        $this->succeed($data);
-    }
-
-    function return_post()
-    {
-        $alipay_config = $this->config->item('alipay');
-        $alipayNotify = new AlipayNotify($alipay_config);
-        //注意：在客户端把返回参数请求过来的时候务必要把sign做一次urlencode,保证"+"号字符不会变成空格。
-        if ($_POST['success'] == "true")//判断success是否为true.
-        {
-            //验证参数是否匹配
-            if (str_replace('"', '', $_POST['partner']) == $alipay_config['partner'] && str_replace('"', '', $_POST['service']) == $alipay_config['service']) {
-
-                //获取要校验的签名结果
-                $sign = $_POST['sign'];
-
-                //除去数组中的空值和签名参数,且把数组所有元素，按照“参数=参数值”的模式用“&”字符拼接成字符串
-                $data = createLinkstring(paraFilter($_POST));
-
-                //logResult('data:'.$data);//调试用，判断待验签参数是否和客户端一致。
-                //logResult('sign:'.$sign);//调试用，判断sign值是否和客户端请求时的一致，
-                $isSgin = false;
-
-                //获得验签结果
-                $isSgin = rsaVerify($data, $alipay_config['alipay_public_key'], $sign);
-                if ($isSgin) {
-                    echo "return success";
-                    //此处可做商家业务逻辑，建议商家以异步通知为准。
-                } else {
-                    echo "return fail";
-                }
-            }
-        }
     }
 
     function isSignVerify($params, $sign)
@@ -147,38 +80,4 @@ class AlipayDao extends BaseDao
         return $alipayNotify->getSignVeryfy($params, $sign);
     }
 
-    function isSignVerify1($params, $sign)
-    {
-        $publicKey = file_get_contents(APPPATH . 'models/alipay/rsa_public_key.pem');
-    }
-
-    function notify_post()
-    {
-        $alipay_config = $this->config->item('alipay');
-        $alipayNotify = new AlipayNotify($alipay_config);
-        if ($alipayNotify->getResponse($_POST['notify_id']))//判断成功之后使用getResponse方法判断是否是支付宝发来的异步通知。
-        {
-            if ($alipayNotify->getSignVeryfy($_POST, $_POST['sign'])) {//使用支付宝公钥验签
-                $out_trade_no = $_POST['out_trade_no'];
-
-                //支付宝交易号
-                $trade_no = $_POST['trade_no'];
-
-                //交易状态
-                $trade_status = $_POST['trade_status'];
-
-                if ($_POST['trade_status'] == 'TRADE_FINISHED') {
-                } else if ($_POST['trade_status'] == 'TRADE_SUCCESS') {
-                }
-                //——请根据您的业务逻辑来编写程序（以上代码仅作参考）——
-                echo "success";        //请不要修改或删除
-            } else //验证签名失败
-            {
-                echo "sign fail";
-            }
-        } else //验证是否来自支付宝的通知失败
-        {
-            echo "response fail";
-        }
-    }
 }
