2ee0472d72a23a461a15e04925dc42fa7533b2dc
lzwjava <lzwjava@gmail.com>
2016-10-04 07:28:54 +0800

    return shareId

--- test/shares_test.go
+++ test/shares_test.go
@@ -16,6 +16,18 @@ func TestShares_create(t *testing.T) {
 	assert.NotNil(t, res)
 }
 
+func TestShares_create_testLive(t *testing.T) {
+	c, _ := NewClientAndUser()
+	liveId := createLive(c)
+
+	c2, _ := NewClientAndUser()
+	res := c2.postData("shares", url.Values{"shareTs": {"1475528321"},
+		"liveId": {liveId}, "channel": {"wechat_timeline"}})
+
+	res = c2.getData("lives/"+liveId, url.Values{})
+	assert.NotNil(t, res.Get("shareId").MustInt() > 0)
+}
+
 func TestShares_create_duplicate(t *testing.T) {
 	c, _ := NewClientAndUser()
 	liveId := createLive(c)
