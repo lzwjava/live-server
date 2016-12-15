618a75bc5b2f67d1fd95f70415717b5065cdbba7
lzwjava <lzwjava@gmail.com>
2016-12-15 21:34:35 +0800

    add MessageQueue code

--- application/controllers/Jobs.php
+++ application/controllers/Jobs.php
@@ -21,4 +21,42 @@ class Jobs extends BaseController
     {
         //$this->statusDao->cleanStatus();
     }
+
+    function queue_get()
+    {
+        $op = $this->get('op');
+        if ($op == 'get') {
+            $this->getMessageQueue();
+        } else {
+            $this->addMessageQueue();
+        }
+    }
+
+    private function getMessageQueue()
+    {
+        $queue = msg_get_queue(TRANSCODE_QUEUE);
+        for (; ;) {
+            $msg = NULL;
+            $msgType = NULL;
+            if (msg_receive($queue, 1, $msgType, 1024, $msg)) {
+                logInfo("receive msg type:" . $msgType . " mgs:" . json_encode($msg));
+            }
+            usleep(1000 * 100);
+        }
+    }
+
+    private function addMessageQueue()
+    {
+        $queue = msg_get_queue(TRANSCODE_QUEUE);
+        $object = new stdclass;
+        $object->name = 'foo';
+        $object->id = uniqid();
+        if (msg_send($queue, 1, $object)) {
+            logInfo("added to queue, stat: " . json_encode(msg_stat_queue($queue)));
+        } else {
+            logInfo("could not add message to queue \n");
+        }
+    }
+
+
 }
