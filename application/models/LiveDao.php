eddd78fff385efc098484f02ecd649c92ca20f2d
lzwjava <lzwjava@gmail.com>
2016-12-23 06:26:53 +0800

    use only live-cdm

--- application/models/LiveDao.php
+++ application/models/LiveDao.php
@@ -161,10 +161,9 @@ class LiveDao extends BaseDao
     private function electFlvServer()
     {
 //        return random_element(array('flv1.quzhiboapp.com:8080', 'flv2.quzhiboapp.com:8080'));
-        return 'live-cdn.quzhiboapp.com';
+        return random_element(array('live-cdn.quzhiboapp.com'));
 //        return 'flv-xycdn.quzhiboapp.com';
 //        return 'cheer.quzhiboapp.com';
-//        return 'video.quzhiboapp.com:8080';
     }
 
     private function electRtmpServer()
