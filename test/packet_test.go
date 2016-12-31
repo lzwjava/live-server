c33f378023a619d52f1b948b2bf20b71eb2f547a
lzwjava <lzwjava@gmail.com>
2017-01-01 00:53:49 +0800

    add packet one

--- test/packet_test.go
+++ test/packet_test.go
@@ -40,3 +40,21 @@ func TestPackets_create_grab(t *testing.T) {
 	grabRes := c2.getData("packets/"+packet.Get("packetId").MustString()+"/grab", url.Values{})
 	assert.NotNil(t, grabRes)
 }
+
+func TestPackets_one(t *testing.T) {
+	c2, userId := NewClientAndUser()
+	insertSnsUser(userId)
+
+	res := c2.post("packets", url.Values{"totalAmount": {"1000"},
+		"totalCount": {"1"}, "channel": {"wechat_h5"}, "wishing": {"新年快乐"}})
+	assert.NotNil(t, res.Interface())
+	orderNo := getLastOrderNo()
+	callbackStr := wechatCallbackStr(orderNo)
+	callbackRes := c2.postWithStr("wechat/wxpayNotify", callbackStr)
+	fmt.Println("callbackRes:" + callbackRes)
+	assert.NotNil(t, callbackRes)
+
+	packet := c2.getData("packets/me", url.Values{})
+	getRes := c2.getData("packets/"+packet.Get("packetId").MustString(), url.Values{})
+	assert.NotNil(t, getRes.Interface())
+}
