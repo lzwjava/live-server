943a1e69f0709b9607e7bb1085572912d8a0acc0
lzwjava <lzwjava@gmail.com>
2017-02-16 17:33:21 +0800

    improve

---
+++ application/models/ParamDao.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: lzw
+ * Date: 2/15/17
+ * Time: 6:11 PM
+ */
+class ParamDao extends BaseDao
+{
+    private $client;
+
+    function __construct()
+    {
+        parent::__construct();
+        $this->client = $this->newRedisClient(1, 'global:');
+    }
+
+    function setTaskRunning($value)
+    {
+        $this->client->set(KEY_TASK_RUNNING, $value);
+    }
+
+    function getTaskRunning()
+    {
+        $taskRunning = $this->client->get(KEY_TASK_RUNNING);
+        if ($taskRunning) {
+            return $taskRunning;
+        } else {
+            return 0;
+        }
+    }
+
+}
