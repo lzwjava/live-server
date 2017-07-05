eb33979ae182b13c8ee420c9cd9e09afdf455dc5
Liu-Sheng Xin <lsx@sksdeMacBook-Pro.local>
2017-07-05 17:56:30 +0800

    Qrcode debug

--- application/controllers/Qrcodes.php
+++ application/controllers/Qrcodes.php
@@ -31,7 +31,7 @@ class Qrcodes extends BaseController
             ->setForegroundColor(array('r' => 0, 'g' => 0, 'b' => 0, 'a' => 0))
             ->setBackgroundColor(array('r' => 255, 'g' => 255, 'b' => 255, 'a' => 0))
             //->setImageType(QrCode::IMAGE_TYPE_PNG);
-            ->setWriterByName('png')
+            ->setWriterByName('png');
         header('Content-Type: ' . $qrcode->getContentType());
         $qrcode->render();
     }
