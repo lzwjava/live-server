774ce0986f1a0193b2c17ce5db18584da6636264
Amast <17783854897@163.com>
2018-05-28 00:04:49 +0800

    修复回放的时间只是片段

--- application/libraries/QiniuLive.php
+++ application/libraries/QiniuLive.php
@@ -21,12 +21,7 @@ class QiniuLive
         try {
             $stream = $this->hub->getStream('z1.qulive.' . $live->rtmpKey);
             $result = $stream->segments();
-//            $start = $result['start'];
-//            $end = $result['end'];
-            $segment0 = $result['segments'][0];
-            $start = $segment0['start'];
-            $end = $segment0['end'];
-            $resp = $stream->saveAs('playback.m3u8', 'm3u8', $start, $end);
+            $resp = $stream->saveAs('playback.m3u8', 'm3u8');
             $playbackUrl = $resp['url'];
             return $playbackUrl;
         } catch (Exception $e) {
